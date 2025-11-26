9a43040544bfd0a057e4de211dea3ab09c8509a1
ShellC-blts <caqv2011@live.com.mx>
2025-11-26 11:24:54 -0700

    continuo

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MenuManager : MonoBehaviour
+{
+    public void JugarComoSoldadoA()
+    {   
+        PlayerPrefs.SetInt("PersonajeSeleccionado", 0);
+        SceneManager.LoadScene("Nivel1");
+    }
+    public void JugarComoSoldadoB()
+    {
+        PlayerPrefs.SetInt("PersonajeSeleccionado", 1);
+        SceneManager.LoadScene("Nivel1");
+    }
+}
