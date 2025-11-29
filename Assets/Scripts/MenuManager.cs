9a4f0a23b030bc6ed59fe4a405905e2c7aa401c5
ShellC-blts <caqv2011@live.com.mx>
2025-11-28 20:48:05 -0700

    Cambios al cargar escenario

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -6,11 +6,13 @@ public class MenuManager : MonoBehaviour
     public void JugarComoSoldadoA()
     {   
         PlayerPrefs.SetInt("PersonajeSeleccionado", 0);
+        Debug.Log("Este boton Funciona");
         SceneManager.LoadScene("Nivel1");
     }
     public void JugarComoSoldadoB()
     {
         PlayerPrefs.SetInt("PersonajeSeleccionado", 1);
+        Debug.Log("Este boton Funciona");
         SceneManager.LoadScene("Nivel1");
     }
 }
